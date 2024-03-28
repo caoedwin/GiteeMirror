dab2314fbe2812a1cf9a4bdeb26bc03275e10f3c
曹泽 <10669375+Cao_Ze@user.noreply.gitee.com>
2024-03-28 11:20:20 +0800

    202403028am

--- Reliability_Row_data/ABODriverTool/apps.py
+++ Reliability_Row_data/ABODriverTool/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class AbodrivertoolConfig(AppConfig):
+    #  不能为名称，应该是app的路径
     name = 'ABODriverTool'
+    # 中文要显示的名称
+    # verbose_name = "博客"
+    # verbose_name_plural = verbose_name
--- Reliability_Row_data/AutoResult/apps.py
+++ Reliability_Row_data/AutoResult/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class AutoresultConfig(AppConfig):
+    #  不能为名称，应该是app的路径
     name = 'AutoResult'
+    # 中文要显示的名称
+    verbose_name = "自动化统计"
+    verbose_name_plural = verbose_name
